74584ae0d2f996426d30ac0017ab9f815887bd0a
chaitanya-maddala-236 <chaitanyaa.2006@gmail.com>
2025-09-07 23:04:19 +0530

    Added new files

---
+++ Basics/func.cpp
@@ -0,0 +1,14 @@
+// A fucntion is a block of code that performs a specific task and can be called upon multiple times 
+
+#include<iostream>
+using namespace std; 
+
+int main(){
+    int n=5;
+
+    
+
+    
+    return 0;
+
+}
