30062ac70af8b368d98ceb496982e3b30812c7ac
chaitanya-maddala-236 <chaitanyaa.2006@gmail.com>
2025-09-08 12:05:45 +0530

    updated

---
+++ NumberTheoryAndMaths/primeNumber.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+bool IfPrime(int n){
+    if (n==1 || n==0){
+        return false;
+    }
+
+    for(int i=2; i<sqrt(n);i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int main(){
+    bool ans =IfPrime(23);
+cout<<ans;
+return 0;
+
+}
